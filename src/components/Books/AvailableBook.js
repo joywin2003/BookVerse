e4adf686125a8821aa028711a428b2c074b1a1a6
joywin2003 <21a20.joywin@sjec.ac.in>
2023-09-06 21:47:13 +0530

    added button

--- src/components/Books/AvailableBook.js
+++ src/components/Books/AvailableBook.js
@@ -2,32 +2,33 @@ import classes from "./AvailableBook.module.css";
 import Card from "../UI/Card";
 import BookItems from "./BookItems/BookItems";
 import CartContext from "../../store/cart-context";
-import { useEffect, useState,useContext } from "react";
+import { useEffect, useState, useContext } from "react";
+import Button from "../UI/Button/Button";
 
 const AvailableBook = () => {
   // const [availableBooks, setAvailableBooks] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
-  const value = useContext(CartContext)
-  const {availableBooks, setAvailableBooks} = value
+  const value = useContext(CartContext);
+  const { availableBooks, setAvailableBooks } = value;
   const fetchBooks = async () => {
     setIsLoading(true);
     setIsError(false);
     try {
-      const response = await fetch('http://127.0.0.1:8000/get_books/');
+      const response = await fetch("http://127.0.0.1:8000/get_books/");
       if (!response.ok) {
-        throw new Error('Something went wrong!');
+        throw new Error("Something went wrong!");
       }
       const data = await response.json();
 
-      const books = data.books.map(book => {
+      const books = data.books.map((book) => {
         const [id, title, author, genre, price] = book;
         return {
           id,
           title,
           author,
           genre,
-          price
+          price,
         };
       });
       setAvailableBooks(books);
@@ -39,24 +40,36 @@ const AvailableBook = () => {
   };
   useEffect(() => {
     fetchBooks();
-  },[]);
+  }, []);
   const BookList = availableBooks.map((book) => (
-    <BookItems key={book.id} id = {book.id} title = {book.title} author = {book.author} genre = {book.genre} price = {book.price} />
+    <BookItems
+      key={book.id}
+      id={book.id}
+      title={book.title}
+      author={book.author}
+      genre={book.genre}
+      price={book.price}
+    />
   ));
 
   let content = <p>Found no Books.</p>;
-  if(availableBooks.length > 0){
-      content = <ul>{BookList}</ul>;
+  if (availableBooks.length > 0) {
+    content = <ul>{BookList}</ul>;
   }
-  if(isLoading){
+  if (isLoading) {
     content = <p>Loading...</p>;
   }
-  if(isError){
+  if (isError) {
     content = <h3>{isError}</h3>;
   }
   return (
     <section className={classes.meals}>
       <Card>
+        <div className={classes.buttonContainer}>
+          <Button className={classes.refresh} onClick={fetchBooks}>
+            Refresh
+          </Button>
+        </div>
         {content}
       </Card>
     </section>
